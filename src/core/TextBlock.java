a4eff74fd71520964d5c4311768b60a1ff344581
Turner Bohlen <turnerbohlen@gmail.com>
2012-10-04 14:20:21 -0400

    Moves text into TextBlock object

--- src/core/TextBlock.java
+++ src/core/TextBlock.java
@@ -4,4 +4,8 @@ public class TextBlock {
     public float[] pos; // x, y
     public String text;
     public float[] size; // width, height
+
+    public TextBlock(String text) {
+        this.text = text;
+    }
 }
--- src/run/MainWindow.java
+++ src/run/MainWindow.java
@@ -4,6 +4,8 @@
  */
 package run;
 
+import core.TextBlock;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -23,11 +25,11 @@ import core.Player;
 public class MainWindow extends Window {
 
     private final int LINE_HEIGHT = 100; // Distance between baselines of successive lines of text.
-    private final double TICK_LENGTH = 10; // Milliseconds for text to move one pixel.
+    private final double TICK_LENGTH = 15; // Milliseconds for text to move one pixel.
     private final int MARGIN = 30;
     private final int SCREEN_WIDTH = 250 - 2 * MARGIN; // FIXME: calculate.
     private BufferedReader reader;
-    private ArrayList<String> lines;
+    private ArrayList<TextBlock> lines;
     private int lineOffset;
     private UnicodeFont font;
 
@@ -36,7 +38,7 @@ public class MainWindow extends Window {
     public MainWindow(Player player) {
         super(player);
         font = new UnicodeFont(java.awt.Font.decode("Arial"));
-        lines = new ArrayList<String>(200);
+        lines = new ArrayList<TextBlock>(200);
     }
 
     @Override
@@ -45,8 +47,8 @@ public class MainWindow extends Window {
         float y = player.windowPos[1];
         g.setColor(Color.black);
         for (int i = 0; i < lines.size(); i++) {
-            String line = lines.get(i);
-            g.drawString(line, x + MARGIN, y + MARGIN + LINE_HEIGHT * i - lineOffset);
+            TextBlock line = lines.get(i);
+            g.drawString(line.text, x + MARGIN, y + MARGIN + LINE_HEIGHT * i - lineOffset);
         }
 
         player.render(container, game, g, mainPlayerPos[0], mainPlayerPos[1]);
@@ -76,12 +78,12 @@ public class MainWindow extends Window {
                     if (font.getWidth(currentString.toString()) > SCREEN_WIDTH) {
                         // FIXME: this will cause problems with one-word lines.
                         int prevLength = currentString.length() - word.length() - 1;
-                        lines.add(currentString.substring(0, prevLength));
+                        lines.add(new TextBlock(currentString.substring(0, prevLength)));
                         currentString.delete(0, prevLength + 1);
                     }
                 }
             }
-            lines.add(currentString.toString());
+            lines.add(new TextBlock(currentString.toString()));
         } catch (IOException e) {
             throw new SlickException(e.getMessage());
         }
