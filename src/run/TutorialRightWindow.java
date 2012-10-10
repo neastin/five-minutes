6d7385986cbd9cacfc14bff6e2b35eed4c8ab2ea
Lasagna220 <Lasagna220@pkt-eighty-six.mit.edu>
2012-10-10 16:17:38 -0400

    Added left and right tutorial minigames

---
+++ src/run/TutorialRightWindow.java
@@ -0,0 +1,48 @@
+package run;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import core.Player;
+
+public class TutorialRightWindow extends Window {
+    private int lapCounter = 0;
+    private int lapGoal;
+
+    public TutorialRightWindow(Player player) {
+        super(player);
+        lapGoal = 3;
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void render(GameContainer container, StateBasedGame game, Graphics g, Player player) throws SlickException {
+        player.render(container, game, g, playerPos[0], playerPos[1]);
+    }
+
+    @Override
+    public void init(GameContainer container, StateBasedGame game, Player player) throws SlickException {
+        playerPos[0] = player.windowPos[0] + player.pWidth;
+        playerPos[1] = player.windowPos[1] + 170;
+    }
+
+    @Override
+    public void update(GameContainer container, StateBasedGame game, int delta, Player player) throws SlickException {
+        Input input = container.getInput();
+
+        float moveValue = delta * .2f;
+        if (input.isKeyDown(player.getButton("right"))) {
+            playerPos[0] += moveValue;
+        }
+        if (playerPos[0] > player.windowPos[0] + player.windowSize[0] - player.pWidth) {
+            lapCounter += 1;
+            playerPos[0] = player.windowPos[0] + player.pWidth;
+        }
+        if (lapCounter >= lapGoal) {
+            this.over = true;
+        }
+    }
+}
